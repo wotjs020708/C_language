680c0ea90efe6d9b26df660bf902261cd917a366
wotjs020708 <67827517+wotjs020708@users.noreply.github.com>
2023-04-13 17:50:45 +0900

    Add files via upload

---
+++ c_4/0413_1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void printf_stars()
+{
+  for (int i = 0; i < 30; i++)
+  {
+    printf("*");
+  }
+  }
+int main(){
+  printf_stars();
+  printf("\nHello World!\n");
+  printf_stars();
+  printf("\n");
+  return 0;
+}
